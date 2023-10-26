fe0a5a977558930e01faf39ad5ed7ce713931e33
tkuchiki <kuchiki.taku@gmail.com>
2023-10-26 20:40:33 +0900

    Add build step to release workflow

---
+++ scripts/ci/archive.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+bin="slp"
+version="${1}"
+
+set -e
+
+cd build
+for file in ./${bin}-* ; do
+    dir=$(echo ${file} | awk -F'-' -v bin=${bin} '{print bin"_"$(NF-1)"_"$(NF)}')
+    mkdir ${dir}
+    mv ${file} ${dir}/${bin}
+    # zip
+    zip ${dir}.zip -j ${dir}/${bin} ../README.md ../LICENSE
+    # tar
+    cd ${dir}
+    cp ../../README.md .
+    cp ../../LICENSE .
+    tar czf ${dir}.tar.gz ${bin} README.md LICENSE
+    mv ${dir}.tar.gz ../
+    cd ../
+    rm -rf ${dir}
+done
+
+shasum -a 256 *.zip > ${bin}_${version}_checksums.txt
